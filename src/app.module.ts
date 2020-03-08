5684f02787fb0cbd82aff7c0fcfe3e8a4ceaf599
James Roncesvalles <roncy.pro13@gmail.com>
2020-03-08 12:42:57 +0800

    remove players module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicTacToeModule } from './tic-tac-toe/tic-tac-toe.module';
-import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/tic_tac_toe'), TicTacToeModule, PlayersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/tic_tac_toe'), TicTacToeModule],
   controllers: [AppController],
   providers: [AppService],
 })
