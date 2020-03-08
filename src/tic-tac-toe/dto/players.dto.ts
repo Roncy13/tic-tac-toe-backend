4ded7dbed922c247245f754fc2ba4d7d323dc6de
James Roncesvalles <roncy.pro13@gmail.com>
2020-03-08 12:54:32 +0800

    added mongodb, added route for player two

--- src/tic-tac-toe/dto/players.dto.ts
+++ src/tic-tac-toe/dto/players.dto.ts
@@ -4,5 +4,5 @@ export class PlayerOneDTO {
 
 export class PlayerTwoDTO {
   playerTwo: string;
-  connectiong: string;
+  connection: string;
 }
--- src/tic-tac-toe/tic-tac-toe.controller.ts
+++ src/tic-tac-toe/tic-tac-toe.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, Body, HttpCode, HttpStatus, Put } from '@nestjs/common';
+import { Response } from 'express';
+import { PlayerOneDTO, PlayerTwoDTO } from './dto/players.dto';
 
 @Controller('tic-tac-toe')
-export class TicTacToeController {}
+export class TicTacToeController {
+
+  @Post("/player-one")
+  async playerOne(
+    @Body() body: PlayerOneDTO,
+    @Res() response: Response): Promise<Response> {
+
+    return response.status(HttpStatus.CREATED).json(body);
+  }
+
+  @Put("/player-two")
+  async playerTwo(
+    @Body() body: PlayerTwoDTO,
+    @Res() response: Response
+  ): Promise<Response> {
+
+    return response.status(HttpStatus.OK).json(body);
+  }
+}
