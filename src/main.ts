9dd5c142d617e99102363dc52197182ce6eb7330
James Roncesvalles <roncy.pro13@gmail.com>
2020-03-10 12:10:35 +0800

    added cors for front end

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: true
   });
+
+  app.use(helmet());
+  const options = {
+    origin: 'http://localhost:8000',
+    methods: 'GET',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  };
+
+  app.enableCors(options);
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalPipes(
